f43a2b15f86ed85a3e6f37e50eb1bda0f22bcddc
fvzwieten <fvanzwie@redhat.com>
2020-01-20 20:56:18 +0100

    Update createuser.ps1

--- createuser.ps1
+++ createuser.ps1
@@ -1,3 +1,3 @@
-$newpwd = ConvertTo-SecureString -String "r3dh4t1!" -AsPlainText -Force
-New-LocalUser -Name "ansible" -Password $newpwd" -PasswordNeverExpires -Description "Account for Ansible Automation"
-Add-LocalGroupMember -Group "Administrators" -Member "ansible"
+$newpwd = ConvertTo-SecureString -String "r3dh4t1!" -AsPlainText -Force 
+New-LocalUser -Name "ansible" -Password $newpwd" -PasswordNeverExpires -Description "Account for Ansible Automation" 
+Add-LocalGroupMember -Group "Administrators" -Member "ansible" 
